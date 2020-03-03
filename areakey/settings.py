4af337f02b0233e416f52d874d8a1dd680730873
ghanshyamdhakar <parthvj1998@gmail.com>
2020-03-03 11:55:14 +0530

    correction in media url

--- areakey/settings.py
+++ areakey/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL ='/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
