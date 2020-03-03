d2629f4f7ea8595459cd64c67e3644288e5b8011
ghanshyamdhakar <parthvj1998@gmail.com>
2020-03-03 10:22:01 +0530

    correction in home view

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
+# <<<<<<< HEAD
 # Create your views here
-=======
+# =======
 def home(request):
 	return render(request, 'home.html')
 
 # Create your views here.
->>>>>>> 0b46415b63e389b21cd085c15be9a517c0f8863f
+# >>>>>>> 0b46415b63e389b21cd085c15be9a517c0f8863f
