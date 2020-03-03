9a2aea68f32f1957914efe59dbd128cb1ab2d07a
ghanshyamdhakar <parthvj1998@gmail.com>
2020-03-03 12:07:11 +0530

    login url create

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
 
 app_name='home'
 
 urlpatterns=[
+	path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 	path('', home, name='home'),
 ]
