b218a2ad05c2be44786c642445075b8f3a3daa2e
ghanshyamdhakar <parthvj1998@gmail.com>
2020-03-06 22:40:18 +0530

    create areakey model

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+import io
 # Create your models here.
 
 gender = (('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other'))
@@ -23,3 +26,25 @@ class User(AbstractUser):
 
 	def __str__(self):
 		return str(self.username)
+
+class AreaKey(models.Model):
+	user=models.ForeignKey(User, on_delete=models.PROTECT)
+	file=models.FileField(null=True)
+	data=models.CharField(max_length=200, blank=True)
+	rowdata=models.CharField(max_length=300, blank=True)
+	complete=models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.file.name
+
+# @receiver(post_save, sender=AreaKey)
+# def areakeyfn(sender, instance, **kwargs):
+# 	filename=instance.file
+# 	path=settings.BASE_DIR + '/media/' + str(filename)
+# 	print(type(path))
+# 	# print(str(filename))
+# 	# fpath=path.encode('utf-8').strip()
+# 	# print(fpath)
+# 	f=io.open(path, 'r', encoding='utf-8')
+# 	content=f.read()
+# 	print(content)
