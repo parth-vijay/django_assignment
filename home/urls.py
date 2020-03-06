d8fa3eb371f97e609ea61161e3f663e5a3718365
ghanshyamdhakar <parthvj1998@gmail.com>
2020-03-06 22:43:05 +0530

    create areakey url

--- home/urls.py
+++ home/urls.py
@@ -6,8 +6,11 @@ from django.contrib.auth.views import LoginView
 app_name='home'
 
 urlpatterns=[
+	path('profile/', user_profile, name='user_profile'),
 	path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+	path('logout/', logout_request, name='logout'),
 	path('product/', product, name='product'),
 	path('areakey/', areakey, name='areakey'),
+	# path('areakey_csv/', csv_file, name='csv_file'),
 	path('', home, name='home'),
 ]
