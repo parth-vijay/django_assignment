474fc1c6cdd85ba287ed4216a739dc8d783586d7
ghanshyamdhakar <parthvj1998@gmail.com>
2020-03-13 20:21:34 +0530

    post_data method create

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from .models import *
 import csv, io
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from .forms import *
 import json
@@ -31,7 +32,7 @@ def areakey(request):
 			if 'file' in request.FILES:
 				profile.file=request.FILES['file']
 			profile.save()
-			return redirect('home:home')
+			return redirect('home:areakey')
 	else:
 		form=AreaKeyForm()
 	rowda=AreaKey.objects.filter(user=request.user.id).values('rowdata')
@@ -43,4 +44,19 @@ def areakey(request):
 def user_profile(request):
 	user=get_object_or_404(User, id=request.user.id)
 	return render(request, 'user_profile.html', {'user':user})
+
+@csrf_exempt
+def table_data(request):
+	data=request.POST.getlist('id[]')
+	for i in data:
+		d=int(i.split('|')[0])
+		j=int(i.split('|')[1])
+		# print(d)
+	print(type(d))
+	rdata=AreaKey.objects.filter(pk=d).values('rowdata')
+	# print(rdata)
+	i=[]
+	rodata=json.loads(str(rdata))
+	print(rodata)
+	return HttpResponse("Success!")
 # Create your views here
